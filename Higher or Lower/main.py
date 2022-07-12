65133a7a0cb4ff08c86f273416c3094405faf1fb
Debojyoti Sarkar <68661525+DebojyotiSarkar21@users.noreply.github.com>
2022-07-12 22:22:25 +0530

    Add files via upload

---
+++ Higher or Lower/main.py
@@ -0,0 +1,52 @@
+from art import logo, vs
+from game_data import data
+import random
+
+
+def format_data(account):
+      account_name = account["name"]
+      account_descr = account["description"]
+      account_country = account["country"]
+      return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+def check_answers(guess, a_followers, b_followers):
+      if a_followers > b_followers:
+            return guess == "a"
+      else:
+            return guess == "b"
+
+
+print(logo)
+score = 0
+game_continue = True
+account_b = random.choice(data)
+
+while game_continue:
+      account_a = account_b
+      account_b = random.choice(data)
+      if account_b == account_a:
+            account_b = random.choice(data)
+
+      print(f" Compare A: {format_data(account_a)}.")
+      print(vs)
+      print(f" Against B: {format_data(account_b)}.")
+      guess = input("Who has more Instagram followers? Type 'A' or 'B': ").lower()
+
+
+      a_follower_count = account_a["follower_count"]
+      b_follower_count = account_b["follower_count"]
+
+      is_correct = check_answers(guess, a_follower_count, b_follower_count)
+
+      ##clear()
+      ##print(logo)
+
+      if is_correct:
+            score += 1
+            print(f"You're Right! currrent score: {score}")
+      else:
+            game_continue = False
+            print((f"Sorry, that's wrong. Final score: {score}"))
+
+
